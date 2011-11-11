6c59acd5457e7d17cd2fcd618425f96390537617
Tom Finegan <tomfinegan@google.com>
2011-11-10 20:45:21 -0500

    Define VideoSinkPin and VideoSinkFilter.
    Stubs/basic initialization only.
    
    Change-Id: Ibcf0930943c2db681699d58a0a82e8e92874b61b

--- http_client/win/video_sink_filter.cc
+++ http_client/win/video_sink_filter.cc
@@ -6,7 +6,9 @@
 // in the file PATENTS.  All contributing project authors may
 // be found in the AUTHORS file in the root of the source tree.
 
-#include "baseclasses/streams.h"
+#include "http_client/win/video_sink_filter.h"
+
+#include "http_client/win/webm_guids.h"
 
 // TODO(tomfinegan): webrtc uses baseclasses, but has worked around the need
 //                   for the next two lines. Determining how to do so would be
@@ -15,5 +17,61 @@
 //                   sources will cause an error at link time (LNK2001,
 //                   unresolved external symbol) because of use of the following
 //                   two globals via extern.
-CFactoryTemplate* g_Templates = NULL;
-int g_cTemplates = 0;
+CFactoryTemplate* g_Templates = NULL;   // NOLINT
+int g_cTemplates = 0;                   // NOLINT
+
+namespace webmlive {
+
+///////////////////////////////////////////////////////////////////////////////
+// VideoSinkPin
+//
+
+VideoSinkPin::VideoSinkPin(TCHAR* ptr_object_name,
+                           VideoSinkFilter* ptr_filter,
+                           CCritSec* ptr_filter_lock,
+                           HRESULT* ptr_result,
+                           LPCWSTR ptr_pin_name)
+    : CBaseInputPin(ptr_object_name, ptr_filter, ptr_filter_lock, ptr_result,
+                    ptr_pin_name) {
+}
+
+VideoSinkPin::~VideoSinkPin() {
+}
+
+HRESULT VideoSinkPin::GetMediaType(int32 type_index,
+                                   CMediaType* ptr_media_type) {
+  return E_NOTIMPL;
+}
+
+HRESULT VideoSinkPin::CheckMediaType(const CMediaType* ptr_media_type) {
+  return E_NOTIMPL;
+}
+
+HRESULT VideoSinkPin::Receive(IMediaSample* ptr_sample) {
+  return E_NOTIMPL;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// VideoSinkFilter
+//
+VideoSinkFilter::VideoSinkFilter(TCHAR* ptr_filter_name,
+                                 LPUNKNOWN ptr_iunknown,
+                                 HRESULT* ptr_result)
+    : CBaseFilter(ptr_filter_name, ptr_iunknown, &filter_lock_,
+                  CLSID_VideoSinkFilter),
+      ptr_sink_pin_(NULL) {
+  *ptr_result = E_FAIL;
+  ptr_sink_pin_ = new VideoSinkPin(NAME("VideoSinkInputPin"), this,
+                                   &filter_lock_, ptr_result, L"VideoSink");
+  if (!ptr_sink_pin_ || FAILED(*ptr_result)) {
+      *ptr_result = FAILED(*ptr_result) ? (*ptr_result) : E_OUTOFMEMORY;
+  }
+}
+
+VideoSinkFilter::~VideoSinkFilter() {
+    delete ptr_sink_pin_;
+    ptr_sink_pin_ = NULL;
+}
+
+}  // namespace webmlive
---
+++ http_client/win/video_sink_filter.h
@@ -0,0 +1,71 @@
+// Copyright (c) 2011 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+
+// Defines the video sink filter used to obtain raw frames from user input
+// devices available via DirectShow. Based on WebRTC's CaptureInputPin and
+// CaptureSinkFilter.
+
+#ifndef HTTP_CLIENT_WIN_VIDEO_SINK_FILTER_H_
+#define HTTP_CLIENT_WIN_VIDEO_SINK_FILTER_H_
+
+#include "baseclasses/streams.h"
+#include "http_client/basictypes.h"
+#include "http_client/http_client_base.h"
+#include "http_client/webm_encoder.h"
+
+namespace webmlive {
+
+// Forward declare |VideoSinkFilter| for use in |VideoSinkPin|.
+class VideoSinkFilter;
+
+class VideoSinkPin : public CBaseInputPin {
+ public:
+  enum {
+    kInvalidArg = E_INVALIDARG,
+    kSuccess = S_OK,
+    kFalse = S_FALSE,
+  };
+  VideoSinkPin(TCHAR* ptr_object_name,
+               VideoSinkFilter* ptr_filter,
+               CCritSec* ptr_filter_lock,
+               HRESULT* ptr_result,
+               LPCWSTR ptr_pin_name);
+  virtual ~VideoSinkPin();
+
+  HRESULT GetMediaType(int32 type_index, CMediaType* ptr_media_type);
+  HRESULT CheckMediaType(const CMediaType* ptr_media_type);
+  STDMETHODIMP Receive(IMediaSample* ptr_sample);
+  HRESULT SetConfig(const WebmEncoderConfig::VideoCaptureConfig& config);
+ private:
+  WebmEncoderConfig::VideoCaptureConfig requested_config_;
+  WebmEncoderConfig::VideoCaptureConfig actual_config_;
+  WEBMLIVE_DISALLOW_COPY_AND_ASSIGN(VideoSinkPin);
+};
+
+class VideoSinkFilter : public CBaseFilter {
+ public:
+  enum {
+    kInvalidArg = E_INVALIDARG,
+    kSuccess = S_OK,
+    kFalse = S_FALSE,
+  };
+  VideoSinkFilter(TCHAR* ptr_filter_name,
+                  LPUNKNOWN ptr_iunknown,
+                  HRESULT* ptr_result);
+  virtual ~VideoSinkFilter();
+ private:
+  // The filter lock. Must be acquired after the receive lock.
+  CCritSec filter_lock_;
+  CCritSec receive_lock_;
+  VideoSinkPin* ptr_sink_pin_;
+  WEBMLIVE_DISALLOW_COPY_AND_ASSIGN(VideoSinkFilter);
+};
+
+}  // namespace webmlive
+
+#endif  // HTTP_CLIENT_WIN_VIDEO_SINK_FILTER_H_
