fb3e07b3f7e18cf72bf32b8eec5fac7b87ca6ba5
Tom Finegan <tomfinegan@google.com>
2011-11-16 17:26:07 -0500

    Add basic VideoFrame implementation.
    Change-Id: I0cd1ac3f2ef93a737b85c3221fcc65ac297aab41

--- http_client/video_encoder.cc
+++ http_client/video_encoder.cc
@@ -5,3 +5,55 @@
 // tree. An additional intellectual property rights grant can be found
 // in the file PATENTS.  All contributing project authors may
 // be found in the AUTHORS file in the root of the source tree.
+
+#include "http_client/video_encoder.h"
+
+namespace webmlive {
+
+VideoFrame::VideoFrame()
+    : width_(0), height_(0), timestamp_(0), buffer_length_(0) {
+}
+
+VideoFrame::~VideoFrame() {
+}
+
+int32 VideoFrame::InitI420(int32 width, int32 height, int64 timestamp,
+                           uint8* ptr_data, int32 data_length) {
+  if (!ptr_data) {
+    return kInvalidArg;
+  }
+  width_ = width;
+  height_ = height;
+  timestamp_ = timestamp;
+  if (data_length > buffer_length_) {
+    buffer_length_ = data_length;
+    buffer_.reset(new (std::nothrow) uint8[buffer_length_]);
+    if (!buffer_) {
+      return kNoMemory;
+    }
+  }
+  memcpy(&buffer_[0], ptr_data, data_length);
+  return kSuccess;
+}
+
+void VideoFrame::Swap(VideoFrame* ptr_frame) {
+  int32 temp = width_;
+  width_ = ptr_frame->width_;
+  ptr_frame->width_ = temp;
+
+  temp = height_;
+  height_ = ptr_frame->height_;
+  ptr_frame->height_ = temp;
+
+  int64 temp_timestamp = timestamp_;
+  timestamp_ = ptr_frame->timestamp_;
+  ptr_frame->timestamp_ = temp_timestamp;
+
+  buffer_.swap(ptr_frame->buffer_);
+
+  temp = buffer_length_;
+  buffer_length_ = ptr_frame->buffer_length_;
+  ptr_frame->buffer_length_ = temp;
+}
+
+}  // namespace webmlive
--- http_client/video_encoder.h
+++ http_client/video_encoder.h
@@ -9,21 +9,40 @@
 #ifndef HTTP_CLIENT_VIDEO_CAPTURE_H_
 #define HTTP_CLIENT_VIDEO_CAPTURE_H_
 
+#include "boost/scoped_array.hpp"
 #include "http_client/basictypes.h"
+#include "http_client/http_client_base.h"
 #include "http_client/video_types.h"
 
 namespace webmlive {
 
 class VideoFrame {
  public:
+  enum {
+    kNoMemory = -2,
+    kInvalidArg = -1,
+    kSuccess = 0,
+  };
   VideoFrame();
   ~VideoFrame();
-
+  // Allocates storage for |ptr_data|, sets internal fields to values in 
+  // callers args, and returns |kSuccess|.
+  int32 InitI420(int32 width, int32 height, int64 timestamp, uint8* ptr_data, 
+                 int32 data_length);
+  // Swaps |VideoFrame| data.
+  void Swap(VideoFrame* ptr_frame);
+  int32 width() const { return width_; }
+  int32 height() const { return height_; }
+  int64 timestamp() const { return timestamp_; }
+  uint8* buffer() const { return buffer_.get(); }
+  int32 buffer_length() const { return buffer_length_; }
  private:
-  I420Frame i420_frame;
-  int64 timestamp;
-  uint8* ptr_data;
-  int32 data_length;
+  int32 width_;
+  int32 height_;
+  int64 timestamp_;
+  boost::scoped_array<uint8> buffer_;
+  int32 buffer_length_;
+  WEBMLIVE_DISALLOW_COPY_AND_ASSIGN(VideoFrame);
 };
 
 class VideoFrameCallback {
@@ -32,7 +51,7 @@ class VideoFrameCallback {
    kDropped = 1,
  };
  public:
-  virtual int OnVideoFrameReceived(VideoFrame* ptr_frame) = 0;
+  virtual int32 OnVideoFrameReceived(VideoFrame* ptr_frame) = 0;
 };
 
 }  // namespace webmlive
